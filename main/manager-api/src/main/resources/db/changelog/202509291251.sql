ab781f1be0dea87c31a5e445005ed2f5f6e73c5d
lapy <vin.lapenta@gmail.com>
2025-09-29 13:00:09 +0100

    Fix migration

--- main/manager-api/src/main/resources/db/changelog/202509291251.sql
+++ main/manager-api/src/main/resources/db/changelog/202509291251.sql
@@ -7,11 +7,11 @@
 -- Update exit commands configuration to include bye and goodbye
 UPDATE `sys_params` 
 SET `param_value` = 'exit;close;bye;goodbye',
-    `update_time` = '2025-09-29 12:51:00'
+    `update_date` = '2025-09-29 12:51:00'
 WHERE `param_code` = 'exit_commands';
 
 -- Update wakeup words configuration with new comprehensive list
 UPDATE `sys_params` 
 SET `param_value` = 'Alexa;Hi ESP;Jarvis;computer;Hey Willow;Sophia;Hey Wanda;Hi Jolly;Hi Fairy;Hey Printer;Mycroft;Hi Joy;Hi Jason;Astrolabe;Hey Ily;Blue Chip;Hi Lily;Hi Telly;Hi Wall E',
-    `update_time` = '2025-09-29 12:51:00'
+    `update_date` = '2025-09-29 12:51:00'
 WHERE `param_code` = 'wakeup_words';
